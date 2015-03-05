8868bf2f8ca70a78a62b7d263013f1aece2e95a6
margotduek <margotduek@gmail.com>
2015-03-05 11:13:26 -0600

    aplicacion de tallercitos de polymer

--- segundo meetup/PolymerApp/starter/app.js
+++ segundo meetup/PolymerApp/starter/app.js
@@ -1,6 +1,16 @@
-
+/*
 var tabs = document.querySelector('paper-tabs');
 
 tabs.addEventListener('core-select', function() {
-  console.log("Selected: " + tabs.selected);
+  console.log("DiegoEsCool: " + tabs.selected);
+});
+*/
+
+
+var tabs = document.querySelector('paper-tabs');
+
+var list = document.querySelector('post-list');
+
+tabs.addEventListener('core-select', function() {
+  list.show = tabs.selected;
 });
